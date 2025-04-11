e4137444e811b18fc081fe7711cc15e1796dea42
Milos Vasic <i@mvasic.ru>
2025-04-11 23:13:07 +0300

    Testing, fixing and polishing.

--- Install.sh
+++ Install.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sh Build.sh && sh sh StartService.sh && sudo docker exec clamav which msmtp && \
-    sudo docker exec clamav sh -c 'echo "Test email" | mailx -s "Test" ${ALERT_EMAIL}'
+sh Build.sh && sh StartService.sh
