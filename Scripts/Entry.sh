61abe0533ee045119f363e7ada062db261f104db
Milos Vasic <i@mvasic.ru>
2025-04-14 16:24:21 +0300

    GenerateMsmtp.

--- Scripts/Entry.sh
+++ Scripts/Entry.sh
@@ -70,18 +70,11 @@ else
     exit 1
 fi
 
-cat <<EOF > /etc/msmtprc
-account default
-host ${SMTP_HOST}
-port ${SMTP_PORT}
-from ${SMTP_USER}
-auth on
-user ${SMTP_USER}
-password ${SMTP_PASSWORD}
-tls on
-tls_starttls on
-logfile /var/log/msmtp.log
-EOF
+if ! sh /Scripts/GenerateMsmtp.sh; then
+    
+    echo "ERROR: GenerateMsmtp.sh script failed"
+    exit 1
+fi
 
 SCRIPT="/Scripts/ScanAndAlert.sh"
 
