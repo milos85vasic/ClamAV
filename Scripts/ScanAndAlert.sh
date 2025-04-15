e5f4a0b0253d0cf10b5e84583cb2be4e3f040c91
Milos Vasic <i@mvasic.ru>
2025-04-15 20:42:43 +0300

    FIXME.

--- Scripts/ScanAndAlert.sh
+++ Scripts/ScanAndAlert.sh
@@ -9,5 +9,6 @@ else
   TAG="$1"
 fi
 
+# FIXME: sudo: /Scripts/Log.sh: command not found
 sudo echo "Anti-Virus scan started from '$TAG'" | sudo /Scripts/Log.sh && \
   sudo clamscan -r --remove /scandir | grep --line-buffered "FOUND" | sudo /Scripts/Log.sh
