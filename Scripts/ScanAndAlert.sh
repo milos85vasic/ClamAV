22624de04fd1c6a7afff088ae0ec39d942f618d0
Milos Vasic <i@mvasic.ru>
2025-04-16 00:23:38 +0300

    Typo.

--- Scripts/ScanAndAlert.sh
+++ Scripts/ScanAndAlert.sh
@@ -11,7 +11,7 @@ fi
 
 if ps -A | grep clamscan; then
 
-  sudo echo "Anti-Virus scan is already running. Requested from '$ATG'" | sudo /Scripts/Log.sh
+  sudo echo "Anti-Virus scan is already running. Requested from '$TAG'" | sudo /Scripts/Log.sh
 
 else
 
