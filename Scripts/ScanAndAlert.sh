2b9422d83fbdfa136dfb3e5f1409073b9d5d3284
Milos Vasic <i@mvasic.ru>
2025-04-15 20:38:34 +0300

    Do sudo.

--- Scripts/ScanAndAlert.sh
+++ Scripts/ScanAndAlert.sh
@@ -9,5 +9,5 @@ else
   TAG="$1"
 fi
 
-sudo echo "Anti-Virus scan started from '$TAG'" | /Scripts/Log.sh && \
-  sudo clamscan -r --remove /scandir | grep --line-buffered "FOUND" | /Scripts/Log.sh
+sudo echo "Anti-Virus scan started from '$TAG'" | sudo /Scripts/Log.sh && \
+  sudo clamscan -r --remove /scandir | grep --line-buffered "FOUND" | sudo /Scripts/Log.sh
