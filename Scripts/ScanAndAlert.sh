a1bc44feec3c359d3238f209186d80063b9e65bf
Milos Vasic <i@mvasic.ru>
2025-04-16 17:53:38 +0300

    The target parameter.

--- Scripts/ScanAndAlert.sh
+++ Scripts/ScanAndAlert.sh
@@ -30,7 +30,7 @@ else
 
   sudo echo "Anti-Virus scan started. Requested from '$TAG'" | sudo /Scripts/Log.sh && \
     sudo echo "The following directories will be scanned: " | sudo /Scripts/Log.sh && \
-    sudo ls -lF /scandir | sudo /Scripts/Log.sh && \
+    sudo ls -lF "$SCANWHAT" | sudo /Scripts/Log.sh && \
     sudo clamscan -r --remove --follow-dir-symlinks=0 --follow-file-symlinks=0 "$SCANWHAT" | grep --line-buffered "FOUND" | sudo /Scripts/Log.sh && \
     sudo echo "Anti-Virus scan completed. Requested from '$TAG'" | sudo /Scripts/Log.sh
 fi
