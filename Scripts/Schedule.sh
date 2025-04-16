4b07b2268124e564e7c8e96b39009ce97842d360
Milos Vasic <i@mvasic.ru>
2025-04-16 17:54:19 +0300

    The target parameter.

--- Scripts/Schedule.sh
+++ Scripts/Schedule.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -w / -it clamav bash -c "echo '0 */6 * * * /Scripts/ScanAndAlert.sh' | crontab -"
+docker exec -w / -it clamav bash -c "echo '0 */6 * * * /Scripts/ScanAndAlert.sh SCHEDULED' | crontab -"
